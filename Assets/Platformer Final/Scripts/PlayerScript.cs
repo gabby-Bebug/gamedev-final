206cdae34004f8a818b0446ef8441af58cda8d26
gabby-Bebug <ms.gabriellebatista@gmail.com>
2026-04-22 16:25:02 -0400

    started player script

--- Assets/Platformer Final/Scripts/PlayerScript.cs
+++ Assets/Platformer Final/Scripts/PlayerScript.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    //left right movement
+    public Rigidbody2D RD;
+    public float movex;
+    public Vector3 movement;
+    //jumping (this will come later, focus on movement)
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
